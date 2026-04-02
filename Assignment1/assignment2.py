548306a4c870f90e7da05b275a98b48c3fb16476
Wocked61 <DylanP@csu.fullerton.edu>
2026-04-02 04:06:06 -0700

    finished the program for the syntax analyzer

--- Assignment1/assignment2.py
+++ Assignment1/assignment2.py
@@ -1,11 +1,16 @@
-# ====== PARSER FOR ASSIGNMENT 2 (Reorganized by Rule #) ======
+# ====== PARSER FOR ASSIGNMENT 2  ======
 
 from assignment1 import do_lexer
-
+output_file = None
+PRINT_PRODUCTIONS = True
 tokens = []
 current = 0
 
-
+def write_output(text):
+    global output_file
+    print(text)
+    if output_file:
+        output_file.write(text + "\n")
 
 # Helper functions
 def current_token():
@@ -18,7 +23,8 @@ def advance():
 
 
 def error(msg):
-    print("Syntax Error:", msg)
+    token = current_token()
+    print(f"Syntax Error: {msg}. Found '{token.lexeme}' of type '{token.type}'")
     exit()
 
 
@@ -31,11 +37,14 @@ def match(expected_lexeme=None, expected_type=None):
     if expected_type and token.type != expected_type:
         error(f"Expected {expected_type} but found {token.type}")
 
+    write_output(f"Token: {token.type}    Lexeme: {token.lexeme}")
     advance()
 
 
 # R1. <Rat26S>
 def Rat26S():
+    if PRINT_PRODUCTIONS:
+        write_output("<Rat26S> -> @ <Opt Function Definitions> @ <Opt Declaration List> @ <Statement List> @")
     match(expected_lexeme='@')
     OptFunctionDefinitions()
     match(expected_lexeme='@')
@@ -48,6 +57,8 @@ def Rat26S():
 
 # R2. <Opt Function Definitions>
 def OptFunctionDefinitions():
+    if PRINT_PRODUCTIONS:
+        write_output("<Opt Function Definitions> -> <Function Definitions> | ε")
     if current_token().lexeme == "function":
         FunctionDefinitions()
     # else empty
@@ -57,6 +68,8 @@ def OptFunctionDefinitions():
 # R3. <Function Definitions>
 
 def FunctionDefinitions():
+    if PRINT_PRODUCTIONS:
+        write_output("<Function Definitions> -> <Function> | <Function> <Function Definitions>")
     Function()
     while current_token().lexeme == "function":
         Function()
@@ -67,6 +80,8 @@ def FunctionDefinitions():
 # function <Identifier> ( <Opt Parameter List> ) <Opt Declaration List> <Body>
 
 def Function():
+    if PRINT_PRODUCTIONS:
+        write_output("<Function> -> function <Identifier> ( <Opt Parameter List> ) <Opt Declaration List> <Body>")
     match(expected_lexeme="function")
     match(expected_type="identifier")
     match(expected_lexeme="(")
@@ -79,6 +94,9 @@ def Function():
 
 # R5. <Opt Parameter List>
 def OptParameterList():
+    if PRINT_PRODUCTIONS:
+        write_output("<Opt Parameter List> -> <Parameter List> | ε")
+
     if current_token().type == "identifier":
         ParameterList()
     # else empty
@@ -86,6 +104,9 @@ def OptParameterList():
 
 # R6. <Parameter List>
 def ParameterList():
+    if PRINT_PRODUCTIONS:
+        write_output("<Parameter List> -> <Parameter> | <Parameter> , <Parameter List>")
+
     Parameter()
     while current_token().lexeme == ",":
         match(expected_lexeme=",")
@@ -95,6 +116,9 @@ def ParameterList():
 # R7. <Parameter>
 # <IDs> <Qualifier>
 def Parameter():
+    if PRINT_PRODUCTIONS:
+        write_output("<Parameter> -> <IDs> <Qualifier>")
+
     IDs()
     Qualifier()
 
@@ -102,8 +126,11 @@ def Parameter():
 # R8. <Qualifier>
 # integer | boolean | real
 def Qualifier():
+    if PRINT_PRODUCTIONS:
+        write_output("<Qualifier> -> integer | boolean | real")
+
     if current_token().lexeme in ["integer", "boolean", "real"]:
-        advance()
+        match(expected_type="keyword")
     else:
         error("Expected type qualifier (integer/boolean/real)")
 
@@ -111,6 +138,9 @@ def Qualifier():
 # R9. <Body>
 # { <Statement List> }
 def Body():
+    if PRINT_PRODUCTIONS:
+        write_output("<Body> -> { <Statement List> }")
+
     match(expected_lexeme="{")
     StatementList()
     match(expected_lexeme="}")
@@ -118,6 +148,9 @@ def Body():
 
 # R10. <Opt Declaration List>
 def OptDeclarationList():
+    if PRINT_PRODUCTIONS:
+        write_output("<Opt Declaration List> -> <Declaration List> | ε")
+
     if current_token().lexeme in ["integer", "boolean", "real"]:
         DeclarationList()
     # else empty
@@ -126,6 +159,9 @@ def OptDeclarationList():
 # R11. <Declaration List>
 # <Declaration> ; { <Declaration> ; }
 def DeclarationList():
+    if PRINT_PRODUCTIONS:
+        write_output("<Declaration List> -> <Declaration> ; { <Declaration> ; }")
+
     Declaration()
     match(expected_lexeme=";")
     while current_token().lexeme in ["integer", "boolean", "real"]:
@@ -136,6 +172,9 @@ def DeclarationList():
 # R12. <Declaration>
 # <Qualifier> <IDs>
 def Declaration():
+    if PRINT_PRODUCTIONS:
+        write_output("<Declaration> -> <Qualifier> <IDs>")
+
     Qualifier()
     IDs()
 
@@ -143,6 +182,9 @@ def Declaration():
 # R13. <IDs>
 # <Identifier> | <Identifier> , <IDs>
 def IDs():
+    if PRINT_PRODUCTIONS:
+        write_output("<IDs> -> <Identifier> | <Identifier> , <IDs>")
+
     match(expected_type="identifier")
     while current_token().lexeme == ",":
         match(expected_lexeme=",")
@@ -152,6 +194,9 @@ def IDs():
 # R14. <Statement List>
 # <Statement> | <Statement> <Statement List>
 def StatementList():
+    if PRINT_PRODUCTIONS:
+        write_output("<Statement List> -> <Statement> | <Statement> <Statement List>")
+
     Statement()
     while current_token().lexeme not in ['}', '@', 'fi', 'otherwise']:
         Statement()
@@ -159,6 +204,10 @@ def StatementList():
 
 # R15. <Statement>
 def Statement():
+    if PRINT_PRODUCTIONS:
+        write_output("<Statement> -> <Compound> | <Assign> | <If> | <Return> | <Print> | <Scan> | <While>")
+
+
     token = current_token()
 
     if token.lexeme == "{":
@@ -182,6 +231,9 @@ def Statement():
 # R16. <Compound>
 # { <Statement List> }
 def Compound():
+    if PRINT_PRODUCTIONS:
+        write_output("<Compound> -> { <Statement List> }")
+
     match(expected_lexeme="{")
     StatementList()
     match(expected_lexeme="}")
@@ -190,6 +242,8 @@ def Compound():
 # R17. <Assign>
 # <Identifier> = <Expression> ;
 def Assign():
+    if PRINT_PRODUCTIONS:
+        write_output("<Assign> -> <Identifier> = <Expression> ;")
     match(expected_type="identifier")
     match(expected_lexeme="=")
     Expression()
@@ -200,6 +254,10 @@ def Assign():
 # if ( <Condition> ) <Statement> fi
 # if ( <Condition> ) <Statement> otherwise <Statement> fi
 def If():
+    if PRINT_PRODUCTIONS:
+        write_output("<If> -> if ( <Condition> ) <Statement> fi | if ( <Condition> ) <Statement> otherwise <Statement> fi")
+
+
     match(expected_lexeme="if")
     match(expected_lexeme="(")
     Condition()
@@ -216,6 +274,8 @@ def If():
 # R19. <Return>
 # return ; | return <Expression> ;
 def Return():
+    if PRINT_PRODUCTIONS:
+        write_output("<Return> -> return ; | return <Expression> ;")
     match(expected_lexeme="return")
     if current_token().lexeme != ";":
         Expression()
@@ -225,6 +285,8 @@ def Return():
 # R20. <Print>
 # write ( <Expression> ) ;
 def Print():
+    if PRINT_PRODUCTIONS:
+        write_output("<Print> -> write ( <Expression> ) ;")
     match(expected_lexeme="write")
     match(expected_lexeme="(")
     Expression()
@@ -235,6 +297,9 @@ def Print():
 # R21. <Scan>
 # read ( <IDs> ) ;
 def Scan():
+    if PRINT_PRODUCTIONS:
+        write_output("<Scan> -> read ( <IDs> ) ;")
+
     match(expected_lexeme="read")
     match(expected_lexeme="(")
     IDs()
@@ -245,6 +310,10 @@ def Scan():
 # R22. <While>
 # while ( <Condition> ) <Statement>
 def While():
+    if PRINT_PRODUCTIONS:
+        write_output("<While> -> while ( <Condition> ) <Statement>")
+
+
     match(expected_lexeme="while")
     match(expected_lexeme="(")
     Condition()
@@ -255,6 +324,8 @@ def While():
 # R23. <Condition>
 # <Expression> <Relop> <Expression>
 def Condition():
+    if PRINT_PRODUCTIONS:
+        write_output("<Condition> -> <Expression> <Relop> <Expression>")
     Expression()
     Relop()
     Expression()
@@ -263,32 +334,44 @@ def Condition():
 # R24. <Relop>
 # == | != | > | < | <= | =>
 def Relop():
+    if PRINT_PRODUCTIONS:
+        write_output("<Relop> -> == | != | > | < | <= | =>")
     if current_token().lexeme in ["==", "!=", ">", "<", "<=", "=>"]:
-        advance()
+        match(expected_lexeme=current_token().lexeme)
     else:
         error("Invalid relational operator")
 
 
 # R25. <Expression>
 def Expression():
+    if PRINT_PRODUCTIONS:
+        write_output("<Expression> -> <Term> | <Term> + <Expression> | <Term> - <Expression>")
+
     Term()
     while current_token().lexeme in ['+', '-']:
-        advance()
+        match(expected_lexeme=current_token().lexeme)
         Term()
 
 
 # R26. <Term>
 def Term():
+    if PRINT_PRODUCTIONS:
+        write_output("<Term> -> <Factor> | <Factor> * <Term> | <Factor> / <Term>")
+
     Factor()
     while current_token().lexeme in ['*', '/']:
-        advance()
+        match(expected_lexeme=current_token().lexeme)
         Factor()
 
 
 # R27. <Factor>
 def Factor():
+    if PRINT_PRODUCTIONS:
+        write_output("<Factor> -> - <Primary> | <Primary>")
+
+
     if current_token().lexeme == '-':
-        advance()
+        match(expected_lexeme='-')
         Primary()
     else:
         Primary()
@@ -297,10 +380,12 @@ def Factor():
 # R28. <Primary>
 # Identifier | Integer | Real | ( <Expression> ) | true | false | Identifier ( <IDs> )
 def Primary():
+    if PRINT_PRODUCTIONS:
+        write_output("<Primary> -> Identifier | Integer | Real | ( <Expression> ) | true | false | Identifier ( <IDs> )")
     token = current_token()
 
     if token.type == "identifier":
-        advance()
+        match(expected_type="identifier")
         if current_token().lexeme == "(":
             match(expected_lexeme="(")
             IDs()
@@ -308,21 +393,21 @@ def Primary():
         return
 
     if token.type == "integer":
-        advance()
+        match(expected_type="integer")
         return
 
     if token.type == "real":
-        advance()
+        match(expected_type="real")
         return
 
     if token.lexeme == "(":
-        advance()
+        match(expected_lexeme="(")
         Expression()
         match(expected_lexeme=")")
         return
 
     if token.lexeme in ["true", "false"]:
-        advance()
+        match(expected_lexeme=token.lexeme)
         return
 
     error("Invalid primary")
@@ -332,10 +417,15 @@ def Primary():
 if __name__ == "__main__":
     filename = input("Enter source filename: ")
 
+    base = filename.split('.')[0]
+    output_file = open(base + "_parser_output.txt", "w", encoding="utf-8") 
+
     tokens = do_lexer(filename)
     current = 0
 
     Rat26S()
 
     if current_token().type == "EOF":
-        print("Syntax Valid")
+        write_output("Syntax Valid")
+
+    output_file.close()
